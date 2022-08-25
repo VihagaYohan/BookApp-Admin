f33799fdbe1daddc8d2d2b1bacf8d5b8cd85f05d
ravindu.sathsara <ravindu.sathsara@inovaitsys.com>
2022-08-25 18:58:50 +0530

    fixed issue

--- src/pages/Books/ViewBook.js
+++ src/pages/Books/ViewBook.js
@@ -4,7 +4,7 @@ import { useParams, Link as RouterLink } from 'react-router-dom';
 const ViewBook = () => {
   const { id } = useParams();
 
-  return <div>ViewBook {id}</div>;
+  return <div>ViewBook </div>;
 };
 
 export default ViewBook;
